2d636a4afa3698bf859aeb98f35b1f0bb792008f
David Avendaño <d.avendano.c@hotmail.com>
2021-09-17 00:18:22 -0500

    Successfully posting tweets

--- WorldTweets/Modules/Home/AddPostViewController.swift
+++ WorldTweets/Modules/Home/AddPostViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Simple_Networking
+import SVProgressHUD
+import NotificationBannerSwift
 
 class AddPostViewController: UIViewController {
     // MARK: - IBOutlets
@@ -16,7 +19,7 @@ class AddPostViewController: UIViewController {
     //MARK: - IBActions
     
     @IBAction func addPostAction(){
-        
+        savePost()
     }
     
     @IBAction func dismissAction(){
@@ -29,6 +32,39 @@ class AddPostViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    private func savePost(){
+        
+        let request = PostTweetsRequest(text: postTextView.text,
+                                        imageUrl: nil,
+                                        videoUrl: nil,
+                                        location: nil)
+        
+        SVProgressHUD.show()
+        
+        SN.post(endpoint: Endpoints.post, model: request) { (response: SNResultWithEntity<Post, ErrorResponse>) in
+            
+            SVProgressHUD.dismiss()
+            
+            switch response {
+                // Success login
+                case .success:
+                    self.dismiss(animated: true, completion: nil)
+                    
+                // Unknown errors
+                case .error(let error):
+                    NotificationBanner(title: "Error",
+                                       subtitle: error.localizedDescription,
+                                       style: .danger).show()
+                   
+                // Errrors from the server
+                case .errorResult(let entity):
+                    NotificationBanner(title: "Error",
+                                    subtitle: entity.error,
+                                    style: .warning).show()
+            }
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
